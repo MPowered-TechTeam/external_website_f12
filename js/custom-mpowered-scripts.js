c4669c92425ec0df16c2220ea66aa396a0637162
Jeff Sallans <jeffsallans@gmail.com>
2013-01-28 20:37:12 -0500

    style fixes

--- js/custom-mpowered-scripts.js
+++ js/custom-mpowered-scripts.js
@@ -10,7 +10,7 @@ function slide_in_team() {
 function slide_in_accomplishments() {
 
 	$("#people-grid").hide();
-	$('#people').html("Accomplishments");
+	$('#people').html("What We've Done");
 	$('.accomplishments').show("slide");
 }
 	
@@ -56,57 +56,7 @@ function slide_in_accomplishments() {
 				elem.data('flipped',true);
 			}
 		});
-					
-// ------------------------ Sticky NavBar ------------------------		
-					 
-		// grab the initial top offset of the navigation 
-		var sticky_navigation_offset_top = $('#sticky_navigation').offset().top;
-		 
-		// our function that decides weather the navigation bar should have "fixed" css position or not.
-		var sticky_navigation = function(){
-			var scroll_top = $(window).scrollTop(); // our current vertical position from the top
-			var scroll_bottom = $(window).scrollTop()+$(window).height();
-			 
-			// if we've scrolled more than the navigation, change its position to fixed to stick to top,
-			// otherwise change it back to relative
-			if (scroll_top > sticky_navigation_offset_top) { 
-				$('#sticky_navigation').css({ 'position': 'fixed', 'top':0, 'left':0 });
-
-			} else {
-				$('#sticky_navigation').css({ 'position': 'relative' }); 
-			}
-
-
-			$('#home').removeClass('selected')
-			$('#who').removeClass('selected')
-			$('#projects').removeClass('selected')
-			$('#people').removeClass('selected')
-			$('#involved').removeClass('selected')
-
-			if ($("#get").offset().top < scroll_bottom)
-				$('#involved').addClass('selected')
-			else if($("#ourteam").offset().top < scroll_bottom)
-				$('#people').addClass('selected')
-			else if($("#proj").offset().top < scroll_bottom)
-				$('#projects').addClass('selected')
-			else if($("#mission").offset().top < scroll_bottom)
-				$('#who').addClass('selected')
-			else
-				$('#home').addClass('selected')
-
-
-
-		};
-		 
-		// run our function on load
-		sticky_navigation();
-		 
-		// and run it again every time you scroll
-		$(window).scroll(function() {
-			 sticky_navigation();
-		});
-			 
-		
+						
 // ------------------------ Misc Page Load Functions ------------------------			
 
 		$('#scrollanimate').localScroll({duration:800});
@@ -140,6 +90,64 @@ function slide_in_accomplishments() {
 			// options
 			itemSelector : '.box-accomp',
 			columnWidth : 0
+		});
+
+		$('.box-accomp').click(function() {
+			if ($(this).attr('link') != null) {
+				window.open(
+				  $(this).attr('link'),
+				  '_blank' // <- This is what makes it open in a new window.
+				);
+			}
+		});
+
+// ------------------------ Sticky NavBar ------------------------		
+					 
+		// grab the initial top offset of the navigation 
+		var sticky_navigation_offset_top = $('#sticky_navigation').offset().top;
+		 
+		// our function that decides weather the navigation bar should have "fixed" css position or not.
+		var sticky_navigation = function(){
+			var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+			var scroll_bottom = $(window).scrollTop()+$(window).height();
+			 
+			// if we've scrolled more than the navigation, change its position to fixed to stick to top,
+			// otherwise change it back to relative
+			if (scroll_top > sticky_navigation_offset_top) { 
+				$('#sticky_navigation').css({ 'position': 'fixed', 'top':0, 'left':0, 'z-index':99});
+
+			} else {
+				$('#sticky_navigation').css({ 'position': 'relative' }); 
+			}
+
+
+			$('#home').removeClass('selected')
+			$('#who').removeClass('selected')
+			$('#projects').removeClass('selected')
+			$('#people').removeClass('selected')
+			$('#involved').removeClass('selected')
+
+			if ($("#get").offset().top < scroll_bottom)
+				$('#involved').addClass('selected')
+			else if($("#ourteam").offset().top < scroll_bottom)
+				$('#people').addClass('selected')
+			else if($("#proj").offset().top < scroll_bottom)
+				$('#projects').addClass('selected')
+			else if($("#mission").offset().top < scroll_bottom)
+				$('#who').addClass('selected')
+			else
+				$('#home').addClass('selected')
+
+
+
+		};
+		 
+		// run our function on load
+		sticky_navigation();
+		 
+		// and run it again every time you scroll
+		$(window).scroll(function() {
+			 sticky_navigation();
 		});
 });
 
